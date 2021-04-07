f144e2101c8da434645b5c4ae327f05095cea736
Mauro Morales <contact@mauromorales.com>
2021-04-07 23:04:32 +0200

    Fix loading order

--- lib/height.rb
+++ lib/height.rb
@@ -1,14 +1,14 @@
 require 'json'
 require 'net/http'
 
-require 'height/api'
-require 'height/client'
-require 'height/response'
-require 'height/list_response'
-require 'height/model'
-require 'height/request'
-
 module Height
+  require 'height/api'
+  require 'height/client'
+  require 'height/response'
+  require 'height/list_response'
+  require 'height/model'
+  require 'height/request'
+  
   class << self
     def new
       Height::Client.new
